6226bf19d08112eaf78bb82539c3573bbfa0c8bc
Acer Helios <Acer Helios@192.168.56.1>
2024-05-07 20:32:11 +0700

    first commit

---
+++ src/java/dal/DAO.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dal;
+
+/**
+ *
+ * @author Acer Helios
+ */
+public class DAO extends DBContext  {
+// các lớp DAO đều extends từ DBContext để truy cập vào DB    
+}
